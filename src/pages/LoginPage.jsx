b01e2092f6f5438c40dbdc04269d601c538621a9
Isis Lin <qweb32114@gmail.com>
2023-08-24 16:06:53 +0800

    feat: add authinput props at login page and signup page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,8 +6,12 @@ import {
 } from 'components/common/auth.styled';
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
+import { useState } from 'react';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <AuthContainer>
       <div>
@@ -16,11 +20,23 @@ const LoginPage = () => {
       <h1>登入 Todo</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          type="text"
+          label="帳號"
+          placeholder="請輸入帳號"
+          value={username}
+          onChange={(username) => setUsername(username)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          type="password"
+          label="密碼"
+          value={password}
+          placeholder="請輸入密碼"
+          onChange={(password) => setPassword(password)}
+        />
       </AuthInputContainer>
       <AuthButton>登入</AuthButton>
       <AuthLinkText>註冊</AuthLinkText>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -6,8 +6,15 @@ import {
 } from 'components/common/auth.styled';
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
+import { useState } from 'react';
 
 const SignUpPage = () => {
+  const [register, setRegister] = useState({
+    username: '',
+    password: '',
+    email: '',
+  });
+
   return (
     <AuthContainer>
       <div>
@@ -16,15 +23,32 @@ const SignUpPage = () => {
       <h1>建立您的帳號</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          type="text"
+          label="帳號"
+          value={register.username}
+          placeholder="請輸入帳號"
+          onChange={(username) => setRegister({ ...register, username })}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          type="email"
+          label="Email"
+          value={register.email}
+          placeholder="請輸入 email"
+          onChange={(email) => setRegister({ ...register, email })}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          type="password"
+          label="密碼"
+          value={register.password}
+          placeholder="請輸入密碼"
+        />
       </AuthInputContainer>
       <AuthButton>註冊</AuthButton>
       <AuthLinkText>取消</AuthLinkText>
