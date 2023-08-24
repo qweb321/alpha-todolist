8cbaf7011a4f0e32538eea587a192e8274778914
Isis Lin <qweb32114@gmail.com>
2023-08-24 16:56:40 +0800

    feat: add login function and show alert

---
+++ src/api/auth.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const authUrl = 'https://todo-list.alphacamp.io/api/auth';
+
+export const login = async ({ username, password }) => {
+  try {
+    const { data } = await axios.post(`${authUrl}/login`, {
+      username,
+      password,
+    });
+    console.log(data);
+
+    const { authToken } = data;
+    if (authToken) {
+      return { success: true, ...data };
+    }
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,11 +7,37 @@ import {
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 import { useState } from 'react';
+import { login } from '../api/auth';
+import Swal from 'sweetalert2';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleClick = async () => {
+    if (username.length === 0 || password.length === 0) {
+      return;
+    }
+    const showWindow = (status, title) => {
+      Swal.fire({
+        position: 'top',
+        title: title,
+        timer: 1000,
+        icon: status,
+        showConfirmButton: false,
+      });
+    };
+
+    const { success, authToken } = await login({ username, password });
+
+    if (success) {
+      localStorage.setItem('authToken', authToken);
+      showWindow('success', '登入成功！');
+      return;
+    }
+
+    showWindow('error', '登入失敗');
+  };
   return (
     <AuthContainer>
       <div>
@@ -38,7 +64,7 @@ const LoginPage = () => {
           onChange={(password) => setPassword(password)}
         />
       </AuthInputContainer>
-      <AuthButton>登入</AuthButton>
+      <AuthButton onClick={handleClick}>登入</AuthButton>
       <AuthLinkText>註冊</AuthLinkText>
     </AuthContainer>
   );
